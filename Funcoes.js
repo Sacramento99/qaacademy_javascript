dc72ef42f82a60ececa51f67b6d045e7415dd4f3
Sacramento99 <luisrodrigosacramento@hotmail.com>
2023-08-24 19:55:00 -0300

    implementação

--- Funcoes.js
+++ Funcoes.js
@@ -40,8 +40,58 @@ function somaComRetorno(a,b,i){
     console.log(somaComRetorno(10,20,30))
 }
 
-function subtracaoComRetorno (l,r,s){
-    const subtracao = l-r-s;
-    return subtracao;
+// function subtracaoComRetorno (l,r,s){
+//     const subtracao = l-r-s;
+//     return subtracao;
+// }
+// console.log(subtracaoComRetorno(10,40,60))
+
+function mult(a,b){
+const mult = a*b;
+return mult;
+
 }
-console.log(subtracaoComRetorno(10,40,60))
+// console.log(mult(52,10))
+
+function imprimirFrase(frase){
+    return frase;
+
+}
+// console.log(imprimirFrase("O amor e lindo"));
+
+function palavrasemretorno(){
+    const palavra ="mousepad"
+    console.log(palavra)
+
+}
+// palavrasemretorno();
+
+function trocarValores(valor1,valor2){
+    
+    const trocar = valor2;
+    valor2 = valor1;
+    valor1 = trocar;;
+    console.log("valor1:" +valor1+ "valor2:" + valor2);
+
+
+}
+// trocarValores(30,20);
+
+function dividiporDois(param){
+    return param;
+
+}
+const valor = dividiporDois(15);
+if(valor/2 == 0){
+    console.log("igual a zero")
+    return true;
+
+}else{
+    
+    console.log("retorno invalido")
+    return false;
+
+}
+
+
+
