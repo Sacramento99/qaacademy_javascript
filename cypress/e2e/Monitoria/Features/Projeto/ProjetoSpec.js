78926fff0f7e05c4280a566eb013b85fba3b000b
Sacramento99 <luisrodrigosacramento@hotmail.com>
2023-10-24 19:39:34 -0300

    Projeto

---
+++ cypress/e2e/Monitoria/Features/Projeto/ProjetoSpec.js
@@ -0,0 +1,46 @@
+/// <reference types="Cypress" />
+import {Given,When,Then} from "cypress-cucumber-preprocessor/steps"
+import { elementos } from "../../Pages/ProjetoPages";
+
+//cenário 1
+Given('que o usuario acessa o site Madeira Madeira', function () {
+    const URL = "https://www.madeiramadeira.com.br";
+    cy.visit(URL);
+});
+
+When('pesquisa um Produto', function () {
+    cy.get(elementos.campobusca,{timeout:5000}).eq(1).click({force:true}).type('prateleiras').type("{ENTER}");
+
+})
+
+Then ('o produto e exibido com sucesso',function(){
+    cy.get(elementos.imagemprateleira,{timeout:5000}).eq(0).should("be.visible")
+
+})
+//cenário 2
+
+When ('adiciona um produto no carrinho', function(){
+    cy.get(elementos.imagemprateleira,{timeout:5000}).eq(0).click({force:true})
+    cy.get(elementos.carrinhoadicionar,{timeout:5000}).eq(0).click({force:true})
+})
+
+Then ('o produto e adicionado com sucesso', function (){
+  cy.get(elementos.itemnocarrinho,{timeout:5000}).eq(0).click({force:true})
+  cy.get(elementos.prateleiracarrinho,{timeout:5000}).eq(1)
+
+})
+
+//cenário 3 
+
+When  ('excluo um produto do carrinho', function (){
+
+})
+
+Then ('a quantidade de produtos fica zerada',function(){
+
+})
+
+
+
+
+
